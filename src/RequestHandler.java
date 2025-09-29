b99aa8d4e0d70ded243b92abd48a6db5c638900e
Madhan3009 <madhankumaar96@gmail.com>
2025-09-29 21:19:34 +0530

    28/09/2025

--- src/RequestHandler.java
+++ src/RequestHandler.java
@@ -5,17 +5,49 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Locale;
+import java.util.Map;
 
 public class RequestHandler {
     private final BufferedReader reader;
     private byte[] bodyBytes;
     private String responseHeaders;
-
+    private String method;
+    private String path;
+    private Map<String,String> requestHeaders;
     public RequestHandler(BufferedReader reader) {
         this.reader = reader;
     }
 
     public void processRequest() throws IOException {
+       serveRequestLine();
+       serveRequestHeaders();
+       handleRequest();
+    }
+
+    private void handleRequest() throws IOException {
+        switch (method.toUpperCase()){
+            case "GET":
+                serveGetRequest(path);
+                break;
+            case "HEAD":
+                serveHeadRequest(path);
+                break;
+            default:
+                sendError(405,"Method Not Allowed");
+        }
+    }
+
+    private void serveHeadRequest(String path) throws IOException {
+        serveFile(path);
+        bodyBytes = new byte[0];
+    }
+
+    private void serveGetRequest(String path) throws IOException {
+        serveFile(path);
+    }
+
+    public void serveRequestLine() throws IOException {
         String requestLine = reader.readLine();
         if (requestLine == null) {
             sendError(400, "Bad Request");
@@ -28,10 +60,27 @@ public class RequestHandler {
             return;
         }
 
-        String method = parts[0];
-        String path = parts[1];
+        method = parts[0];
+        path = parts[1];
 
         serveFile(path);
+    }
+    public void serveRequestHeaders() throws IOException{
+        String headerline;
+        while((headerline = reader.readLine())!=null){
+            if(headerline.isEmpty()){
+                break;
+            }
+        }
+        int index = headerline.indexOf(':');
+        if(index>0){
+            String name = headerline.substring(0,index).trim();
+            String value = headerline.substring(index+1).trim();
+            requestHeaders.put(name.toLowerCase(),value);
+        }
+        System.out.println(STR."Method: \{method}");
+        System.out.println(STR."Host: \{requestHeaders.get("host")}");
+        System.out.println(STR."User-agent: \{requestHeaders.get("user-agent")}");
     }
 
     private void serveFile(String path) throws IOException {
